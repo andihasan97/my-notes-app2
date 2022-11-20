c7a8bb5d6706438aa6f4698e1afe3d308610b54f
andihasan97 <andihasanelfalakiy@gmail.com>
2022-11-20 21:04:29 +0700

    Latihan app catatan dg SQLite

---
+++ app/src/main/java/com/andihasan7/mynotesapp2/helper/MappingHelper.kt
@@ -0,0 +1,23 @@
+package com.andihasan7.mynotesapp2.helper
+
+import android.database.Cursor
+import com.andihasan7.mynotesapp2.db.DatabaseContract
+import com.andihasan7.mynotesapp2.entity.Note
+
+object MappingHelper {
+
+    fun mapCursorToArrayList(notesCursor: Cursor?): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+
+        notesCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                notesList.add(Note(id, title, description, date))
+            }
+        }
+        return notesList
+    }
+}
